0a66d1653c7f5124e5efca00ae88effa885c0069
Emmet <emmetlawton@gmail.com>
2020-02-08 20:22:44 +0000

    insert one and insert many

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,15 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// CRUD - Create Read Update Delete
 
+
+/* const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID */
+
+// Destructured syntax
+const { MongoClient, ObjectID } = require('mongodb')
+
+const id = new ObjectID()
+console.log(id.getTimestamp())
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = '2-task-app-tryout'
 
@@ -9,10 +18,48 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
         return console.log('Unable to connnect to database')
     }
     const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: "Andrew",
+    // Insert one 
+   /*  db.collection('users').insertOne({
+        _id: id,
+        name: "Buloo",
         age: 27
-    })
+    }, (error, result) => {
+        if (error){
+            return console.log('Unagle to insert user')
+        }
+        console.log(result.ops)
+    })  */
+
+    // Insert Two
+    /* db.collection('users').insertMany([
+        {name: "Jen", age: 23},{name: "Frank", age: 24}
+    ],(error, result) => {
+        if (error){
+            return console.log("unable to insert documents")
+        }
+        console.log(result.ops)
+    }) */
+
+    // Exercise
+    /* db.collection('tasks').insertMany([
+        {
+            description: "wash dishes",
+            completed: false
+        },
+        {
+            description: "Clean clothes",
+            completed: false
+        },
+        {
+            description: "take out trash",
+            completed: false
+        }
+    ],(error, result) => {
+        if(error){
+            return console.log(error)
+        }
+        console.log(result.ops)
+    }) */
 })
 
 
