2f6122ae0b5c9f9f06ea2012eef18c21826c5aca
Emmet <emmetlawton@gmail.com>
2020-02-09 20:25:25 +0000

    Update documents in database

--- mongodb.js
+++ mongodb.js
@@ -18,6 +18,7 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
         return console.log('Unable to connnect to database')
     }
     const db = client.db(databaseName)
+    // Create
     // Insert one 
    /*  db.collection('users').insertOne({
         _id: id,
@@ -60,6 +61,81 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
         }
         console.log(result.ops)
     }) */
+
+    // Read)
+    // Find One
+  /*   db.collection('users').findOne({ _id: new ObjectID("5e3df913a06aa40777b3e138") }, (err, user) => {
+        if(err){
+            return console.log('unable to fetch')
+        }
+
+        console.log(user)
+    }) */
+    
+    // Find multiple documents
+  /*   db.collection('users').find({ age: 27 }).toArray((error, users) => {
+        console.log(users)
+    })
+    db.collection('users').find({ age: 27 }).count((error, count) => {
+        console.log(count)
+    }) */
+
+
+    // Chalenge 
+   /*  db.collection('tasks').findOne({ _id: new ObjectID("5e3ea516e1737a06db68a179") }, (error, task) => {
+        if(error){
+            return console.log(error)
+        }
+
+        console.log(task)
+    })
+
+    db.collection('tasks').find({completed: false}, (err, tasks) => {
+        if(err){
+            return console.log(err)
+        }
+
+        console.log(tasks)
+    }) */
+
+    // Update
+    // Update One
+   /*  db.collection('users').updateOne({
+        _id: new ObjectID("5e3df913a06aa40777b3e138")
+    }, {
+         $inc: {
+            age: 1
+         }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    }) */
+
+    // Update Many
+   /*  db.collection('users').updateMany({
+        age: 28
+    }, {
+        $mul: {
+            age: 0.5
+        }
+    }).then((result) =>{
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    }) */
+    // Update Challenge
+    db.collection('tasks').updateMany({
+        completed: false
+    }, {
+        $set: {
+            completed: true
+        }
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
 })
 
 
