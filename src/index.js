085eef1086124f29640107509d051111b0189415
Emmet <emmetlawton@gmail.com>
2020-02-16 16:57:12 +0000

    update user

--- src/index.js
+++ src/index.js
@@ -62,6 +62,26 @@ app.get('/users/:id', async (req, res) => {
     }) */
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const udpates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = update.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+    if(!isValidOperation){
+        return res.status(400).send({'error': "invalid updates!"})
+    }
+    try{
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!user){
+            return res.status(404).send()
+        } 
+        res.send(user)
+    } catch(e){
+        res.status(400).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     try{
